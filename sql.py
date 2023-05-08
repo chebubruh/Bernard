6a542f9c68cc186b9fab6dea4ed497957779e718
Denis <denis.chbgn@gmail.com>
2023-05-08 20:11:44 +0300

    добавил защиту от sql-инъекций в остальных функциях

--- sql.py
+++ sql.py
@@ -15,7 +15,7 @@ def table_clearing(chat_id):
     with connect(user=config.user, password=config.password, host=config.host, database=config.database,
                  port=config.port) as db:
         cur = db.cursor()
-        cur.execute(f'''DELETE FROM "{chat_id}"''')
+        cur.execute('''DELETE FROM "%s"''', (chat_id,))
 
 
 def insert_into_table_values(chat_id, first_name, values):
@@ -23,13 +23,13 @@ def insert_into_table_values(chat_id, first_name, values):
                  port=config.port) as db:
         cur = db.cursor()
         cur.execute(
-            f'''INSERT INTO "{chat_id}" ({first_name.lower()}) VALUES (%s)''', (values,))
+            '''INSERT INTO "%s" ("%s") VALUES (%s)''', (AsIs(chat_id), AsIs(first_name.lower()), values))
 
 
 def select_data(first_name, chat_id):
     with connect(user=config.user, password=config.password, host=config.host, database=config.database,
                  port=config.port) as db:
         cur = db.cursor()
-        cur.execute(f'''SELECT {first_name.lower()} FROM "{chat_id}"''')
+        cur.execute(f'''SELECT %s FROM "%s"''', (AsIs(first_name.lower()), AsIs(chat_id)))
         a = cur.fetchall()
     return a
