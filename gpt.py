6333f8a79c02d0084d92e0f34d521d66272c68b6
Denis <denis.chbgn@gmail.com>
2023-05-30 21:52:26 +0300

    вынес ответ gpt в отдельный файл, добавил зависимости

--- gpt.py
+++ gpt.py
@@ -1,23 +1,28 @@
 import openai_async
+import asyncio
 import config
 import openai
 import json
 import sql
 
 
-async def gpt(chat_id, first_name, content):
+async def gpt(chat_id, first_name, content):  # content должен быть message.text
     try:
         await sql.create_table(chat_id, first_name.split()[0])  # Попытка создания таблицы, если она еще не существует
     except:
         pass
 
-    messages = [{"role": "system", "content": "Ты интеллигентный, вежливый виртуальный помощник по имени Бернард"}]
-    json_from_user = json.dumps({"role": "user", "content": content})
-    await sql.insert_into_table_values(chat_id, first_name.split()[0], json_from_user)
+    messages = [{"role": "system",
+                 "content": "Ты интеллигентный, вежливый виртуальный помощник по имени Бернард"}]  # список для контекста
+
+    json_from_user = await asyncio.to_thread(json.dumps, {"role": "user",
+                                                          "content": content})  # Преобразование текста пользователя в JSON
+    await sql.insert_into_table_values(chat_id, first_name.split()[0],
+                                       json_from_user)  # Вставка сообщения пользователя в таблицу
 
     for i in await sql.select_data(first_name.split()[0], chat_id):
         for j in i:
-            messages.append(j)
+            messages.append(j)  # Получение предыдущих сообщений из таблицы и добавление их в список
 
     for token in config.TOKEN_GPT:
         try:
@@ -26,24 +31,28 @@ async def gpt(chat_id, first_name, content):
                 timeout=None,
                 payload={
                     "model": "gpt-3.5-turbo",
-                    "messages": [{"role": "user", "content": content}],
+                    "messages": messages,
                 },
-            )
+            )  # Запрос к модели GPT-3.5 Turbo для получения ответа
+            response = completion.json()['choices'][0]['message']['content']  # Получение ответа от модели
         except openai.error.AuthenticationError:
             print("AuthenticationError")
             continue
         except openai.error.RateLimitError:
             print("RateLimitError")
             continue
+        except KeyError:
+            print("KeyError: 'choices'")
+            continue
         except openai.error.InvalidRequestError:
             print("InvalidRequestError")
             await sql.table_clearing(chat_id)  # Очистка таблицы от предыдущих данных
+            return "InvalidRequestError"
             break
 
-        response = completion.json()['choices'][0]['message']['content']  # Получение ответа от модели
-        json_from_assistant = json.dumps({"role": "assistant", "content": response})  # Преобразование ответа в JSON
+        json_from_assistant = await asyncio.to_thread(json.dumps, {"role": "assistant",
+                                                                   "content": response})  # Преобразование ответа в JSON
         await sql.insert_into_table_values(chat_id, first_name.split()[0],
                                            json_from_assistant)  # Вставка ответа ассистента в таблицу
         print(f'{messages}\n')
-        break
         return response
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import config
 import openai
 import json
 import sql
+import gpt
 import os
 
 bot = Bot(token=config.TOKEN)
@@ -15,9 +16,8 @@ async def start(message: types.Message):
     await bot.send_message(chat_id=message.chat.id, text='К вашим услугам, сэр!')
 
     try:
-        await sql.create_table(message.chat.id,
-                               message.chat.first_name.split()[
-                                   0])  # Попытка создания таблицы, если она еще не существует
+        await sql.create_table(message.chat.id, message.chat.first_name.split()[
+            0])  # Попытка создания таблицы, если она еще не существует
     except:
         await sql.table_clearing(message.chat.id)  # Очистка таблицы от предыдущих данных
 
@@ -28,55 +28,22 @@ async def chat(message: types.Message):
                                 text='Обработка запроса...')  # Отправка сообщения о начале обработки запроса
 
     try:
-        await sql.create_table(message.chat.id,
-                               message.chat.first_name.split()[
-                                   0])  # Попытка создания таблицы, если она еще не существует
+        await sql.create_table(message.chat.id, message.chat.first_name.split()[
+            0])  # Попытка создания таблицы, если она еще не существует
     except:
         pass
 
-    messages = [{"role": "system",
-                 "content": "Ты интеллигентный, вежливый виртуальный помощник по имени Бернард"}]  # список для контекста
+    answer = await gpt.gpt(message.chat.id, message.chat.first_name, message.text)
 
-    content = message.text
-    json_from_user = json.dumps({"role": "user", "content": content})  # Преобразование текста пользователя в JSON
-    await sql.insert_into_table_values(message.chat.id, message.chat.first_name.split()[0],
-                                       json_from_user)  # Вставка сообщения пользователя в таблицу
-
-    for i in await sql.select_data(message.chat.first_name.split()[0], message.chat.id):
-        for j in i:
-            messages.append(j)  # Получение предыдущих сообщений из таблицы и добавление их в список
-
-    for token in config.TOKEN_GPT:
-        openai.api_key = token
-
-        try:
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages
-            )  # Запрос к модели GPT-3.5 Turbo для получения ответа
-        except openai.error.AuthenticationError:
-            print("AuthenticationError")
-            continue
-        except openai.error.RateLimitError:
-            print("RateLimitError")
-            continue
-        except openai.error.InvalidRequestError:
-            print("InvalidRequestError")
-            await sql.table_clearing(message.chat.id)  # Очистка таблицы от предыдущих данных
-            await bot.edit_message_text(chat_id=message.chat.id, message_id=m1.message_id,
-                                        text='Упс, моя база данных переполнена. К сожалению мне придется стереть себе память, чтобы функицонировать дальше.')  # Редактирование сообщения
-            await bot.send_message(chat_id=message.chat.id,
-                                   text='Пожалуйста, повторите свой запрос, но помните, что я не знаю, о чем мы говорили ранее.')  # Отправка сообщения
-            break
-
-        response = completion.choices[0].message.content  # Получение ответа от модели
-        json_from_assistant = json.dumps({"role": "assistant", "content": response})  # Преобразование ответа в JSON
-        await sql.insert_into_table_values(message.chat.id, message.chat.first_name.split()[0],
-                                           json_from_assistant)  # Вставка ответа ассистента в таблицу
+    if answer == "InvalidRequestError" or answer is None:
         await bot.edit_message_text(chat_id=message.chat.id, message_id=m1.message_id,
-                                    text=f'{response}')  # Редактирование сообщения с ответом
-        print(f'{messages}\n')
-        break
+                                    text='Упс, моя база данных переполнена. К сожалению мне придется стереть себе память, чтобы функицонировать дальше.')  # Редактирование сообщения
+        await bot.send_message(chat_id=message.chat.id,
+                               text='Пожалуйста, повторите свой запрос, но помните, что я не знаю, о чем мы говорили ранее.')  # Отправка сообщения
+        await sql.table_clearing(message.chat.id)
+    else:
+        await bot.edit_message_text(chat_id=message.chat.id, message_id=m1.message_id,
+                                    text=f'{answer}')  # Редактирование сообщения с ответом
 
 
 @dp.message_handler(content_types=['voice'])
@@ -145,4 +112,4 @@ async def voice(message: types.Message):
         break
 
 
-executor.start_polling(dp)  # Запуск бота для прослушивания сообщений
+executor.start_polling(dp, skip_updates=True)  # Запуск бота для прослушивания сообщений
